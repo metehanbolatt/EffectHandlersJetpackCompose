46ec715462e424116948b307f3e8989792adf738
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-02-25 00:49:48 +0300

    SideEffect

---
+++ app/src/main/java/com/metehanbolat/effecthandlersjetpackcompose/sideeffect/SideEffectDemo.kt
@@ -0,0 +1,11 @@
+package com.metehanbolat.effecthandlersjetpackcompose.sideeffect
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+
+@Composable
+fun SideEffectDemo(nonComposeCounter: Int) {
+    SideEffect {
+        println("Called after every successful recomposition")
+    }
+}
