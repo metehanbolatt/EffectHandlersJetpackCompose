e3ec6d99bf9ef7256b6f8edac67e439b0c9d3d9d
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-02-25 11:16:38 +0300

    snapshotFlow

---
+++ app/src/main/java/com/metehanbolat/effecthandlersjetpackcompose/snapshotflow/SnapshotFlowDemo.kt
@@ -0,0 +1,22 @@
+package com.metehanbolat.effecthandlersjetpackcompose.snapshotflow
+
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.mapNotNull
+
+@Composable
+fun SnapshotFlowDemo() {
+    val scaffoldState = rememberScaffoldState()
+    LaunchedEffect(key1 = scaffoldState) {
+        snapshotFlow { scaffoldState.snackbarHostState }
+            .mapNotNull { it.currentSnackbarData?.message }
+            .distinctUntilChanged()
+            .collect { message ->
+                println("A snackbar with message $message was shown")
+            }
+    }
+}
